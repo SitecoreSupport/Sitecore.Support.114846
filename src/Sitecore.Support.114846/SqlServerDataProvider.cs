a5d8cc07b3e6b62dc40565a15ddd1bc10604e78a
dk\ivd <ivd@sitecore.net>
2017-01-25 14:07:52 +0800

    Public property has been added to the SqlServerEventQueue to get last process stamp

---
+++ src/Sitecore.Support.114846/SqlServerDataProvider.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Sitecore.Support.Data.SqlServer
+{
+    using Eventing;
+    using Sitecore.Eventing;
+
+    public class SqlServerDataProvider : Sitecore.Data.SqlServer.SqlServerDataProvider
+    {
+        public SqlServerDataProvider(string connectionString) : base(connectionString)
+        {
+        }
+
+        public override EventQueue GetEventQueue()
+        {
+            return new SqlServerEventQueue(base.Api, base.Database);
+        }
+    }
+}
